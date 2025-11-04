0c60192a785edf4097125b91ac8da3786f76b3e8
highbee2810 <ibrahimkolade2810@gmail.com>
2025-11-04 10:55:32 +0100

    Creating IAM roles and attaching them to Codepipleine and autosclaing group

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -106,3 +106,168 @@ resource "aws_route_table" "public_subnet_rt" {
     subnet_id      = aws_subnet.public_subnet2.id
     route_table_id = aws_route_table.public_subnet_rt.id
   }
+  # creating security group
+resource "aws_security_group" "ASG" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Saas_Web_SG"
+  }
+}
+# creation of IAM Role for Auto Scaling Group
+resource "aws_iam_role" "asg_role" {
+  name = "Saas_ASG_Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+resource "aws_iam_policy" "admin_policy" {
+  name        = "AdminAccessPolicy"
+  description = "Grants full administrative access"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_admin_policy" {
+  role       = aws_iam_role.asg_role.name
+  policy_arn = aws_iam_policy.admin_policy.arn
+}
+# creation of IAM Role for CodeBuild
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-service-role"
+
+  # This policy allows the CodeBuild service to assume this role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "codebuild-role"
+  }
+}
+resource "aws_iam_policy" "codebuild_github_policy" {
+  name        = "CodeBuildGitHubAccess"
+  description = "Allows CodeBuild to access GitHub and interact with CodePipeline and CodeDeploy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # GitHub source access via CodePipeline
+      {
+        Effect = "Allow"
+        Action = [
+          "codepipeline:GetPipeline",
+          "codepipeline:GetPipelineExecution",
+          "codepipeline:StartPipelineExecution",
+          "codepipeline:PollForJobs",
+          "codepipeline:PutJobSuccessResult",
+          "codepipeline:PutJobFailureResult",
+          "codepipeline:AcknowledgeJob"
+        ]
+        Resource = "*"
+      },
+      # CodeBuild permissions to run builds triggered by CodePipeline
+      {
+        Effect = "Allow"
+        Action = [
+          "codebuild:BatchGetProjects",
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds",
+          "codebuild:ListSourceCredentials"
+        ]
+        Resource = "*"
+      },
+      # GitHub OAuth credentials access
+      {
+        Effect = "Allow"
+        Action = [
+          "codebuild:ImportSourceCredentials",
+          "codebuild:DeleteSourceCredentials"
+        ]
+        Resource = "*"
+      },
+      # CodeDeploy permissions for deployment
+      {
+        Effect = "Allow"
+        Action = [
+          "codedeploy:GetDeploymentConfig",
+          "codedeploy:RegisterApplicationRevision",
+          "codedeploy:CreateDeployment",
+          "codedeploy:GetApplicationRevision",
+          "codedeploy:GetDeployment",
+          "codedeploy:GetDeploymentGroup"
+        ]
+        Resource = "*"
+      },
+      # Logging and monitoring
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "codebuild_custom_policy_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.codebuild_github_policy.arn
+}
